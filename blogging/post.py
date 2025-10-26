6d28656e607766683c6bb4493abbb90a637084f7
Aislinn Low <azolinlow@icloud.com>
2025-10-26 15:57:28 -0700

    Added Post class and functions

---
+++ blogging/post.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+class post:
+
+    def __init__(self, code, title, text, creation, update):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.creation = datetime.now()
+        self.update = datetime.now()
+
+    def __str__(self):
+        # returns printable string representation of a post
+        return f"Last Edited: {self.update}. Title: {self.title}. Post Content: {self.text}"
+
+    def __repr__(self):
+        # returns string representation of a post
+        return f"Integer Code: {self.code}. Created: {self.creation}. Updated: {self.update}. Title: {self.title}. Post Content: {self.text}"
+
+    def __eq__(self, other):
+        # compares post based on code, title, text
+        return (self.code == other.code) and (self.title == other.title) and (self.text == other.text)
