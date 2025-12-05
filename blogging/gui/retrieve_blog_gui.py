1d77426c664c36a91f80c28b4e4ff6bc7094afdd
Valeria Gomez <valeriagmezlpez@uvic.ca>
2025-12-04 19:23:50 -0800

    fixed retrieve blogs

---
+++ blogging/gui/retrieve_blog_gui.py
@@ -0,0 +1,66 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QPushButton, QWidget, QLabel, QLineEdit, QMessageBox, QStackedWidget, QPlainTextEdit, QTableView
+from PyQt6.QtGui import QStandardItemModel, QStandardItem
+from blogging.controller import Controller 
+
+from blogging.exception.invalid_login_exception import InvalidLoginException
+from blogging.exception.duplicate_login_exception import DuplicateLoginException
+from blogging.exception.invalid_logout_exception import InvalidLogoutException
+from blogging.exception.illegal_access_exception import IllegalAccessException
+from blogging.exception.illegal_operation_exception import IllegalOperationException
+from blogging.exception.no_current_blog_exception import NoCurrentBlogException
+
+class RetrieveBlogsGUI: 
+    
+    def __init__(self, controller): 
+        
+        self.controller = controller 
+        
+        # retrieve blogs by name layout 
+        self.retrieve_blogs_layout = QGridLayout()
+
+        self.retrieve_blogs_label = QLabel("Search by Name")
+        self.retrieve_blogs_text = QLineEdit()
+        self.retrieve_blogs_btn = QPushButton("Retrieve")
+        self.retrieved_blogs = QTableView()
+        self.go_back_btn4 = QPushButton("Go Back to Main Menu")
+
+        self.retrieve_blogs_layout.addWidget(self.retrieve_blogs_label, 0,0)
+        self.retrieve_blogs_layout.addWidget(self.retrieve_blogs_text, 0,1)
+        self.retrieve_blogs_layout.addWidget(self.retrieve_blogs_btn, 0,2)
+        self.retrieve_blogs_layout.addWidget(self.retrieved_blogs, 1,0)
+        self.retrieve_blogs_layout.addWidget(self.go_back_btn4, 2,0)
+        
+
+        self.retrieve_blogs_widget = QWidget()
+        self.retrieve_blogs_widget.setLayout(self.retrieve_blogs_layout)
+    
+    def load_table(self): 
+        
+        self.retrieved_blogs.setModel(None)
+
+        keyword = self.retrieve_blogs_text.text()
+
+        blogs = self.controller.retrieve_blogs(keyword)
+        try:
+            if len(blogs) > 0:
+                table = QStandardItemModel(len(blogs), 4)
+                
+                table.setHorizontalHeaderLabels(["Id", "Name", "url", "email"])
+                
+                for row, blog, in enumerate(blogs): 
+                    table.setItem(row, 0, QStandardItem(str(blog.id)))
+                    table.setItem(row, 1, QStandardItem(blog.name))
+                    table.setItem(row, 2, QStandardItem(blog.url))
+                    table.setItem(row, 3, QStandardItem(blog.email))
+                
+                self.retrieved_blogs.setModel(table) 
+                self.retrieved_blogs.resizeColumnsToContents()
+
+            else:
+                QMessageBox.warning(None, "Error", "No matching Blogs")
+                self.retrieve_blogs_text.setText("")
+                
+        except IllegalAccessException:
+            QMessageBox.warning(self, "Error", "You Must Login First")
+            self.stack.setCurrentIndex(0)
